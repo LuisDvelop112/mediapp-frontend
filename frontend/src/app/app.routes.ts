f2f3fd4399083d1cc14eca888c7fc39e56af3a5c
LuisDvelop112 <crashluis2221@gmail.com>
2025-10-28 15:16:08 -0500

    Inicializacion de Algunos componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginCardComponent } from './components/login-card/login-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginCardComponent },
+  // ...otras rutas después
+];
